06a99627e5a8fc4d7b168aec5aa8fe3cc428ddb6
elvashenyutong <elvashen@gmail.com>
2024-04-29 04:18:35 -0400

    modify table

--- final.py
+++ final.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import os
+import pandas as pd
 
 st.set_page_config(
     page_title="CSE 6431 final",
@@ -14,16 +15,18 @@ sales_data = {
     "Sales": ["116,591", "281,241", "29,621", "40,875", "468,328"]
 }
 
-st.write("SALES DATA FOR APPLE ACCESSORIES ON AMAZON")
-st.table(sales_data)
+df = pd.DataFrame(sales_data)
 
+st.header("Sales Data for Apple Accessories on Amazon")
+st.markdown(df.to_html(index=False, justify='left'), unsafe_allow_html=True)
+st.title("")
 option_to_images = {
     "Apple USB SuperDrive": ["superdrive.png", "Apple_USB_SuperDrive.png"],
     "Apple USB Ethernet Adapter": ["ethernet_adapter.png"],
     "Apple USB-C to SD Card Reader": ["sd_card_reader.png", "Apple_USB-C_to_SD_Card_Reader.png"],
     "Apple MagSafe 2 Power Adapter": ["magsafe_power_adapter.png", "Apple_60W_MagSafe_2_PowerAdapter.png", "Apple_85W_MagSafe_2_PowerAdapter.png", "Apple_MagSafe_2_Power_Adapter_2lines.png", "Apple_MagSafe_2_Power_Adapter_comb.png"]
 }
-
+st.header("Apple Accessories that will become obsolete")
 selected_option = st.selectbox('Select the I/O device', list(option_to_images.keys()))
 
 image_filenames = option_to_images[selected_option]
